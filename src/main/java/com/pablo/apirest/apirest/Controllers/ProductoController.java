85a541489362e2196433e6ebc948d87756af5950
pabloperezaguilar5 <pabloperezaguilar@gmail.com>
2024-05-02 18:44:57 -0300

    first commit

---
+++ src/main/java/com/pablo/apirest/apirest/Controllers/ProductoController.java
@@ -0,0 +1,57 @@
+package com.pablo.apirest.apirest.Controllers;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pablo.apirest.apirest.Repositories.ProductoRepository;
+import com.pablo.apirest.apirest.Entities.Producto;
+
+
+@RestController
+@RequestMapping("/productos")
+public class ProductoController {
+
+    @Autowired
+    private ProductoRepository productoRepository;
+
+    @GetMapping
+    public List<Producto> getAllProductos(){
+        return productoRepository.findAll();
+    }
+
+    @GetMapping("/{id}")   
+    public Producto getProductoById(@PathVariable Long id) {
+        return productoRepository.findById(id).orElseThrow(() -> new RuntimeException("Producto no encontrado"));
+    }
+    
+    @PostMapping
+    public Producto createProducto(@RequestBody Producto producto) {
+        return productoRepository.save(producto);
+    }
+
+    @PutMapping("/{id}")
+    public Producto updateProducto(@PathVariable Long id, @RequestBody Producto productdetails) {
+        Producto producto = productoRepository.findById(id)
+        .orElseThrow(() -> new RuntimeException("Producto no encontrado"));
+        producto.setNombre(productdetails.getNombre());
+        producto.setPrecio(productdetails.getPrecio());
+        return productoRepository.save(producto);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteProducto(@PathVariable Long id) {
+        Producto producto = productoRepository.findById(id)
+        .orElseThrow(() -> new RuntimeException("Producto no encontrado"));
+        productoRepository.delete(producto);
+        return "el producto con el ID: " + id + " ha sido eliminado";
+    }
+
+}
---
+++ src/main/java/com/pablo/apirest/apirest/Repositories/ProductoRepository.java
@@ -0,0 +1,7 @@
+package com.pablo.apirest.apirest.Repositories;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.pablo.apirest.apirest.Entities.Producto;
+
+public interface ProductoRepository extends JpaRepository <Producto, Long>{
+
+}
